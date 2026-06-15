433790c3539e9e83d285a3e5311d55a9326f5b18
Muhammad Khurram <khurram@eightbitsolutions.com>
2026-06-15 06:43:02 -0700

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -2,8 +2,8 @@
 // The anon key is safe to use in frontend code only when Row Level Security is enabled.
 // Never put a Supabase service-role key in this file.
 window.DOGE_SUPABASE_CONFIG = {
-  url: "https://tchmvutmcbbfilcebuqj.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjaG12dXRtY2JiZmlsY2VidXFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2Mjk1NDAsImV4cCI6MjA5NTIwNTU0MH0.e8ysbeXhps6UJB5UAckGK7N79BByvrl4wE2563LfbTg",
+  url: "",
+  anonKey: "",
   restaurantId: "sakura-sushi-tokyo",
   tableName: "reservations",
 };
